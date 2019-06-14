81360616f147988068e7245b26b4ba9eb8599c2c
Scott Herold <sherold@mail.usf.edu>
2019-06-13 20:38:25 -0400

    First Commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,13 @@
+// Class name must match filename
+public class FirstJavaProgram {
+
+    // Entry method
+    public static void main(String[] args) {
+        
+        // System.out.println() logs in the console
+        System.out.println("My Name is Scott");
+        System.out.println("I am 32 Years old");
+        System.out.println("My hometown is Odenton, MD");
+
+    }
+}
